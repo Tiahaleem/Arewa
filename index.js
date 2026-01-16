38e8fed2b871d622f90ac49f56a075e63704563a
Tia haleem <150017855+Tiahaleem@users.noreply.github.com>
2026-01-17 00:58:01 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -5,3 +5,13 @@
   hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
   });
+  // On click Portfolio Section
+  const buttons = document.querySelectorAll('.portfolio_category_flex button');
+
+  buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      buttons.forEach(b => b.classList.remove('active'));
+      btn.classList.add('active');
+    });
+  });
+
